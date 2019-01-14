95f1020cf3c12142abc9837e58b30bbb3879105e
Niranjan Kala <niranjan.kala@gmail.com>
2019-01-15 00:47:07 +0530

    binding and pips

--- Angular/Angular-GettingStarted/src/app/products/product-list.component.ts
+++ Angular/Angular-GettingStarted/src/app/products/product-list.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 export class ProductListComponent implements OnInit {
     constructor() { }
     title: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter : string = 'cart';
     products: any[]  = [
         {
           "productId": 1,
@@ -60,5 +64,8 @@ export class ProductListComponent implements OnInit {
         }
       ];
     ngOnInit() { }
+    toggleImage(): void {
+      this.showImage = !this.showImage;
+    }
 }
 
